a5f8c0edadf5a5da8947a05510da33a72ac6f001
Stacy985 <anastasiya.disy@gmail.com>
2023-04-18 18:56:44 +0300

    js

--- src/components/index.js
+++ src/components/index.js
@@ -5,24 +5,16 @@ const facebok = document.querySelector(".header__facebok");
 const insta = document.querySelector(".header__insta ");
 const like = document.querySelector(".header__like");
 const price = document.querySelector(".price__title");
-const text = document.querySelector(".price__subtitle");
-const button = document.querySelector(".button");
+const texts = document.querySelectorAll(".price__subtitle");
+const buttons = document.querySelectorAll(".button");
 const photoLarge = document.querySelector(".photo__large");
-const photoPrew = document.querySelector(".photo__prew");
-const iconInButton = document.querySelector(".price__icon");
+const photoPrews = document.querySelector(".photo__prew");
+const iconInButtons = document.querySelector(".price__icon");
 const links = document.querySelectorAll(".link");
 const buttonAdd = document.querySelector(".price__add");
-// links.forEach( {
-//   themeButton.addEventListener("click", function () {
-//     link.classList.toggle("dark-theme");
-//   });
-// });
+const logo = document.querySelector(".header__logo");
+const item = document.querySelector(".footer__item");
 
-/* function darkTheme () {
-  const darkMode = localStorage.getItem('page_dark-theme') === true;
-  localStorage.setItem('page_dark-theme', !darkMode);
-  page.classList.toggle('page_dark-theme', !darkMode);
-} */
 
 themeButton.addEventListener("click", () => {
   page.classList.toggle("dark-theme");
@@ -31,15 +23,19 @@ themeButton.addEventListener("click", () => {
   like.classList.toggle("dark-theme");
   themeButton.classList.toggle("dark-theme");
   price.classList.toggle("dark-theme");
-  text.classList.toggle("dark-theme");
-  button.classList.toggle("dark-theme");
+  item.classList.toggle("dark-theme")
   photoLarge.classList.toggle("dark-theme");
-  photoPrew.classList.toggle("dark-theme");
   search.classList.toggle("dark-theme");
-  iconInButton.classList.toggle("dark-theme");
+  text.classList.toggle("dark-theme");
   buttonAdd.classList.toggle("dark-theme");
-
+  logo.classList.toggle("dark-theme");
 
   Array.from(links).map((link) => link.classList.toggle('dark-theme'));
+  Array.from(buttons).map((button) => button.classList.toggle('dark-theme'));
+
 
 });
+//button.classList.toggle("dark-theme");
+//photoPrew.classList.toggle("dark-theme");
+// iconInButton.classList.toggle("dark-theme");
+// text.classList.toggle("dark-theme");
